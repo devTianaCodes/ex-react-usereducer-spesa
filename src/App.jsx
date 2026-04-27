a44c346c3cd3647cd526bfe3a867942d0a7a6600
SwitchTia <oblasser.tatiana@gmail.com>
2026-04-27 15:11:18 +0200

    Feat: Milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function App() {
   
   const products = [
@@ -7,6 +9,33 @@ export default function App() {
     { name: "Pasta", price: 0.7 },
   ];
 
+  const[addedProducts, setAddedProducts] = useState([]);
+  console.log(addedProducts);
+
+  const addToCart = product => {
+    const isProductAlreadyAdded = addedProducts.some(
+      addedProduct => addedProduct.name === product.name
+    );
+
+    if (isProductAlreadyAdded) {
+      setAddedProducts(curr =>
+        curr.map(addedProduct =>
+          addedProduct.name === product.name
+            ? { ...addedProduct, quantity: addedProduct.quantity + 1 }
+            : addedProduct
+        )
+      );
+      return;
+    }
+
+    setAddedProducts(curr => [
+      ...curr,
+      {
+        ...product,
+        quantity: 1
+      }
+    ]);
+  }
 
   return(
     <>
@@ -17,10 +46,23 @@ export default function App() {
           products.map((product, index) =>(
             <li key={index}>
               <p>{product.name}  ({product.price.toFixed(2)})</p>
+              <button onClick={() => addToCart(product)}>Add to cart</button>
             </li>
           ))
         }
       </ul>
+
+      {addedProducts.length > 0 && (<>
+        <h1>Your cart</h1>
+        <ul>
+          {addedProducts.map((p,i) => (
+            <li key={i}>
+              <p>{p.quantity} * {p.name} ({p.price.toFixed(2) + "euro"})</p>
+
+            </li>
+          ))}
+        </ul>
+      </>)}
     </>
   )
 }
