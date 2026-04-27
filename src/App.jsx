48f007a69faa09bff027e6df75704e6e01c23235
SwitchTia <oblasser.tatiana@gmail.com>
2026-04-27 14:28:37 +0200

    Feat: style css

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,26 @@
 export default function App() {
-  return <h1>React ready</h1>
+  
+  const products = [
+    { name: "Apple", price: 0.5 },
+    { name: "Bread", price: 1.2 },
+    { name: "Milk", price: 1.0 }, 
+    { name: "Pasta", price: 0.7 },
+  ];
+
+
+  return(
+    <>
+      <h1>Choose any product</h1>
+
+      <ul>
+        {
+          products.map((product, index) =>(
+            <li key={index}>
+              <p>{product.name}  ({product.price.toFixed(2)})</p>
+            </li>
+          ))
+        }
+      </ul>
+    </>
+  )
 }
